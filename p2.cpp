345b9b9a7b227c74c5900cde6373cb73ceae8839
aashaykk <aashaykamble911@gmail.com>
2025-08-12 15:07:50 +0530

    Update p2.cpp

--- p2.cpp
+++ p2.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int month;
+
+    int months[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+    cout << "Enter the number between 1-12"<< endl;
+    cin>> month;
+
+    cout << "The number of days for the chosen month is: "<<months[month-1]<<endl;
+
+    return 0;
+}
 
