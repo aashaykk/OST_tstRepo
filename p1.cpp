34839ddf7825c33503b1fa1cda072d5ab1a1eef0
aashaykk <akkamble_b24@it.vjti.ac.in>
2025-08-12 14:53:51 +0530

    added code

---
+++ p1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n = 5;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < i+1; j++) {
+            if(j%2==0) {
+                cout << "0";
+            } else {
+                cout << "1";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
